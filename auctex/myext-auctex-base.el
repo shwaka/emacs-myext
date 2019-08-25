c50e8d914d69030ddc49bec2eaa1faed9b6c4752
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-08-25 21:40:37 +0900

    replace setq with defvar

--- auctex/myext-auctex-base.el
+++ auctex/myext-auctex-base.el
@@ -1,12 +1,15 @@
 ;;; Matches if???, where ??? is an almost arbitrary string.
 ;;; Doesn't match iff, since this is not an if-condition
-(setq myext-auctex-base--if-regexp
-      (rx (seq "if" (or ""
-                        (>= 2 (char alpha "@"))
-                        (char "a-eg-zA-Z@")))
-          word-boundary))
+(defvar myext-auctex-base--if-regexp
+  (rx (seq "if" (or ""
+                    (>= 2 (char alpha "@"))
+                    (char "a-eg-zA-Z@")))
+      word-boundary)
+  "\\ifhoge をインデントするために追加した変数")
 ;; (setq myext-auctex-base--begin-regexp "\\(?:begin\\|ifx\\|ifnum\\|ifdefined\\)\\b")
-(setq myext-auctex-base--else-regexp (rx "else" word-boundary))
+(defvar myext-auctex-base--else-regexp
+  (rx "else" word-boundary)
+  "\\else をインデントするために追加した変数")
 (setq LaTeX-end-regexp (rx (or "end" "else" "fi") word-boundary)) ; これだけは元々auctexに存在
 
 (provide 'myext-auctex-base)
