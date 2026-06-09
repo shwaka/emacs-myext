a17013c48b9ae289bcaa8ad4e1dc0e46290b506d
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-06-09 19:15:52 +0900

    refactor: rewrite myext-auctex-error.el

--- auctex/myext-auctex-error.el
+++ auctex/myext-auctex-error.el
@@ -1,91 +1,87 @@
+;;; myext-auctex-error.el --- Highlight mode-line on AUCTeX error -*- lexical-binding: t; -*-
+
 ;;; auctex のコンパイルエラーを，分かりやすく通知する
 ;;; - mode line が赤く光る
-;;; - post-command-hook を使っているので，何もしなければ赤いまま
-;;; - エラー発生から3秒間は，操作したとしても赤いまま
+;;; - post-command-hook で，操作した後に赤色を消す
 
-;;; error 時
-(defun override:TeX-TeX-sentinel-check (process name)
-  "Cleanup TeX output buffer after running TeX.
-Return nil ifs no errors were found."
-  (save-excursion
-    (goto-char (point-max))
-    (cond
-     ((and (string-match "ConTeXt" name) (boundp 'ConTeXt-Mark-version)
-	   (with-current-buffer TeX-command-buffer
-	     (string= ConTeXt-Mark-version "IV")))
-      (when (re-search-backward " > result saved in file: \\(.*?\\), " nil t)
-	(let ((output-file (TeX-match-buffer 1)))
-	  ;; Shave off quotation marks if present.
-	  (when (string-match "\\`\"\\(.*\\)\"\\'" output-file)
-	    (setq output-file (match-string 1 output-file)))
-	  (setq TeX-output-extension
-		(if (string-match "\\.\\([^.]*\\)$" output-file)
-		    (match-string 1 output-file)
-		  "dvi")))
-	(if (re-search-forward ", \\([0-9]+\\) shipped pages, " nil t)
-	    (setq TeX-current-page (concat "{" (TeX-match-buffer 1) "}")))))
-     (t
-      (if (re-search-backward "^Output written on \\(.*?\\) (\\([0-9]+\\) page"
-			      nil t)
-	  (let ((output-file (TeX-match-buffer 1)))
-	    (setq TeX-current-page (concat "{" (TeX-match-buffer 2) "}"))
-	    ;; Shave off quotation marks if present.
-	    (when (string-match "\\`\"\\(.*\\)\"\\'" output-file)
-	      (setq output-file (match-string 1 output-file)))
-	    (setq TeX-output-extension
-		  (if (string-match "\\.\\([^.]*\\)$" output-file)
-		      (match-string 1 output-file)
-		    "dvi")))))))
-  (if process (TeX-format-mode-line process))
-  (if (re-search-forward "^\\(!\\|.*:[0-9]+:\\) " nil t)
-      (progn
-        (myext-auctex-error--highlight)
-	(message "%s errors in `%s'. Use %s to display." name (buffer-name)
-		 (substitute-command-keys
-		  "\\<TeX-mode-map>\\[TeX-next-error]"))
-	(setq TeX-command-next TeX-command-default)
-	;; error reported to TeX-error-report-switches
-	(setq TeX-error-report-switches
-	      (plist-put TeX-error-report-switches
-			 (intern (plist-get TeX-error-report-switches
-					    'TeX-current-master))
-			 t))
-	t)
-    (let (dvi2pdf)
-      (if (with-current-buffer TeX-command-buffer
-	    (and TeX-PDF-mode (setq dvi2pdf (TeX-PDF-from-DVI))))
-	  (setq TeX-command-next dvi2pdf)
-        (setq TeX-command-next TeX-command-Show)))
-    nil))
-(advice-add 'TeX-TeX-sentinel-check :override 'override:TeX-TeX-sentinel-check)
-(defvar myext-auctex-error--remap-list nil)
-(defun myext-auctex-error--add-remap (face key value)
-  (push (cons
-         (current-buffer)
-         (face-remap-add-relative face key value))
-        myext-auctex-error--remap-list))
-(defun myext-auctex-error--highlight ()
-  "エラー発生を目立たせる"
-  (let ((orig-bg (face-background 'mode-line)))
-    ;; (set-face-background 'mode-line "red")
-    ;; (run-at-time "1 sec" nil
-    ;;              (lambda (bg)
-    ;;                (set-face-background 'mode-line bg))
-    ;;              orig-bg)
-    (with-current-buffer TeX-command-buffer
-      (myext-auctex-error--add-remap 'mode-line :background "red")
-      (myext-auctex-error--add-remap 'mode-line-inactive :background "red")
-      (run-at-time "3 sec" nil
-                   (lambda ()
-                     (add-hook 'post-command-hook #'myext-auctex-error--remove-remaps))))))
-(defun myext-auctex-error--remove-remaps ()
-  (dolist (buf-cookie myext-auctex-error--remap-list)
-    (let ((buf (car buf-cookie))
-          (cookie (cdr buf-cookie)))
-      (with-current-buffer buf
-        (face-remap-remove-relative cookie))))
-  (setq myext-auctex-error--remap-list nil)
-  (remove-hook 'post-command-hook #'myext-auctex-error--remove-remaps))
+(require 'tex)
+(require 'face-remap)
 
+(defgroup myext-auctex-error nil
+  "Notify AUCTeX errors by highlighting the mode-line."
+  :group 'TeX)
+
+(defcustom myext-auctex-error-background "red"
+  "Background color used to notify AUCTeX errors."
+  :type 'color)
+
+(defvar-local myext-auctex-error--remap-cookies nil
+  "Face-remap cookies for the current buffer.")
+
+(defvar myext-auctex-error--highlighted-buffers nil
+  "Buffers whose mode-line is currently highlighted.")
+
+(defun myext-auctex-error--remove-from-buffer (buffer)
+  "Remove mode-line highlighting from BUFFER."
+  (when (buffer-live-p buffer)
+    (with-current-buffer buffer
+      (dolist (cookie myext-auctex-error--remap-cookies)
+        (face-remap-remove-relative cookie))
+      (setq myext-auctex-error--remap-cookies nil)
+      (force-mode-line-update))))
+
+(defun myext-auctex-error-clear ()
+  "Remove all AUCTeX error mode-line highlighting."
+  (interactive)
+  (dolist (buffer myext-auctex-error--highlighted-buffers)
+    (myext-auctex-error--remove-from-buffer buffer))
+  (setq myext-auctex-error--highlighted-buffers nil)
+  (remove-hook 'post-command-hook #'myext-auctex-error-clear)
+  (force-mode-line-update t))
+
+(defun myext-auctex-error--highlight-buffer (buffer)
+  "Highlight mode-line of BUFFER until the next command finishes."
+  (when (buffer-live-p buffer)
+    ;; 既に赤くなっている場合は、いったん古い remap を消す。
+    (myext-auctex-error--remove-from-buffer buffer)
+    (with-current-buffer buffer
+      (push (face-remap-add-relative
+             'mode-line
+             :background myext-auctex-error-background)
+            myext-auctex-error--remap-cookies)
+      (push (face-remap-add-relative
+             'mode-line-inactive
+             :background myext-auctex-error-background)
+            myext-auctex-error--remap-cookies)
+      ;; Emacs 29 以降などでは mode-line-active もあり得る。
+      (when (facep 'mode-line-active)
+        (push (face-remap-add-relative
+               'mode-line-active
+               :background myext-auctex-error-background)
+              myext-auctex-error--remap-cookies))
+      (cl-pushnew buffer myext-auctex-error--highlighted-buffers)
+      (force-mode-line-update)))
+  ;; 次のコマンド終了時に解除する。
+  ;; 何も操作しなければ赤いまま。
+  (add-hook 'post-command-hook #'myext-auctex-error-clear))
+
+(defun myext-auctex-error--latexmk-command-p (name)
+  "Return non-nil if AUCTeX command NAME should trigger notification."
+  (string= name "LatexMk"))
+
+(defun myext-auctex-error--TeX-TeX-sentinel-check-around (orig-fun process name)
+  "Highlight mode-line if ORIG-FUN reports an error for LatexMk."
+  (let ((has-error (funcall orig-fun process name)))
+    (when (and has-error
+               (myext-auctex-error--latexmk-command-p name)
+               (boundp 'TeX-command-buffer)
+               (buffer-live-p TeX-command-buffer))
+      (myext-auctex-error--highlight-buffer TeX-command-buffer))
+    has-error))
+
+(with-eval-after-load 'tex
+  (advice-add 'TeX-TeX-sentinel-check
+              :around
+              #'myext-auctex-error--TeX-TeX-sentinel-check-around))
 
 (provide 'myext-auctex-error)
